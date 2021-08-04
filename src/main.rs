0c0a11b35ce3462e1fdb141b67effaeee4bb9c6a
magpaccio <73834898+magpaccio@users.noreply.github.com>
2021-08-04 21:35:49 +0900

    add

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use rand::Rng;
 
+fn sphere(x :&[f64])-> f64
+{
+    1.0
+}
+
+
 /// # 個体
 /// # field
 /// * `feval` - 現在の評価値
@@ -19,43 +25,6 @@ struct Particle
     pbest_f :f64,
     pbest_pos :Vec<f64>,
 }
-impl particle
-{
-    fn particle_new() 
-    {
-        
-        let n_number = 100_usize;
-        let feval = 0.0_f64;
-        let mut vel = Vec::<f64>::with_capacity(n_number);
-        let mut pos = Vec::<f64>::with_capacity(n_number);
-        let mut init_vel = Vec::<f64>::with_capacity(n_number);
-        let mut init_pos = Vec::<f64>::with_capacity(n_number);
-        let mut pbest_f = 0.0_f64;
-        let mut pbest_pos = Vec::<f64>::with_capacity(n_number);
-
-        for _ in 0..n_number  
-        {
-            let mut rng = rand::thread_rng(); 
-            let rand: i32 = rng.gen();
-            vel.push(0.0);
-            pos.push(0.0);
-            init_vel.push(vec![rand ; n_number]);
-            init_pos.push(vec![rand ; n_number]);
-            pbest_pos.push(0.0);
-        }
-        Self
-        {
-            feval,
-            vel,
-            pos,
-            init_vel,
-            init_pos,
-            pbest_f,
-            pbest_pos,
-        }
-    }
-
-}
 /// # 群れ
 /// # field
 /// * `particles` - 個体群：
@@ -71,17 +40,64 @@ struct Pso
 }
 impl Pso
 {
+    fn particle_new(n_number:usize) 
+    {
+        let feval = 0.0_f64;
+        let mut vel = Vec::<f64>::with_capacity(n_number);
+        let mut pos = Vec::<f64>::with_capacity(n_number);
+        let mut init_vel = Vec::<f64>::with_capacity(n_number);
+        let mut init_pos = Vec::<f64>::with_capacity(n_number);
+        let mut pbest_f = 0.0_f64;
+        let mut pbest_pos = Vec::<f64>::with_capacity(n_number);
+
+        for _ in 0..n_number  
+        {
+            let mut rng = rand::thread_rng(); 
+            let rand: f64 = rng.gen();
+            vel.push(0.0);
+            pos.push(0.0);
+            init_vel.push(vec![rand ; n_number]);
+            init_pos.push(vec![rand ; n_number]);
+            pbest_pos.push(0.0);
+        }
+    }
     
-    fn step_opt()
+    fn step_opt(n_number:usize)
     {
         let mut rng = rand::thread_rng(); 
         let rand: i32 = rng.gen();
 
-        for t in 0..t_max {
-        vel[t + 1] = interia * vel[t] + acc1_coef * rand * (pbest_pos[t] - pos[t]) + acc2_coef * rand *(gbest_pos[t] - pos[t]);
-        pos[t + 1] = pos[t] + vel[t + 1];
+        for i in 0..n_number
+        {
+            vel[i + 1] = interia * vel[i] + acc1_coef * rand * (pbest_pos[i] - pos[i]) + acc2_coef * rand *(gbest_pos[i] - pos[i]);
+            pos[i + 1] = pos[i] + vel[i + 1];
 
         }
+
+        for i in 0..n_number 
+        {
+            feval = sphere(pos[i]);
+
+            if pbest_pos[i] > feval 
+            {
+                for n in 0..N 
+                {
+                    pbest_pos[i + i * n] = pos[i + i * n];
+                }
+                pbest_pos[i] = feval;
+            }
+
+            if gbest_f > feval 
+            {
+                gbest_f = feval;
+                for n in 0..N 
+                {
+                    gbest_f[n] = pos[i + i * n];                    
+                }    
+            }
+            
+        }
+
     }
     
     fn opt()
@@ -96,6 +112,7 @@ impl Pso
 /// * `n_number` - 個体数
 /// * `inertia` -慣性重み 
 /// * `acc_coef` - 加速係数
+/// * `N` - 次元
 struct PsoParam
 {
     t_max :i32,
@@ -103,6 +120,7 @@ struct PsoParam
     inertia :f64,
     acc1_coef :f64,
     acc2_coef :f64,
+    N : i32,
 }
 
 fn main() {
