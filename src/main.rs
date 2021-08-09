3ac69e5b408bf36d456b505f94a54bc1a5a99968
magpaccio <73834898+magpaccio@users.noreply.github.com>
2021-08-10 01:07:46 +0900

    add

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,67 @@ struct Particle
     feval :f64,
     vel :Vec<f64>,
     pos :Vec<f64>,
-    init_vel :Vec<f64>,
-    init_pos :Vec<f64>,
     pbest_f :f64,
     pbest_pos :Vec<f64>,
 }
+impl Particle
+{
+    fn new(n_number:usize)// -> Self
+    {
+        let feval = 0.0_f64;
+        let mut vel = Vec::<f64>::with_capacity(n_number);
+        let mut pos = Vec::<f64>::with_capacity(n_number);
+        let mut init_vel = Vec::<f64>::with_capacity(n_number);
+        let mut init_pos = Vec::<f64>::with_capacity(n_number);
+        let mut pbest_f = 0.0_f64;
+//        let pbest_pos = Vec::new();
+
+        let mut rng = rand::thread_rng(); 
+        for _ in 0..n_number  
+        {
+            vel.push(0.0);
+            pos.push(0.0);
+            init_vel.push(rng.gen::<f64>());
+            init_pos.push(rng.gen::<f64>());
+        }
+/*
+        Self
+        {
+            feval,
+            vel,
+            pos,
+            pbest_f,
+            pbest_pos,
+        }*/
+    }
+    
+    fn ref_mut(&mut self) -> (&mut Vec<f64>, &mut Vec<f64>, &mut Vec<f64>)
+    {
+        (&mut self.pos, &mut self.vel, &mut self.pbest_pos)
+    }
+
+    fn step_pos(&mut self) 
+    {
+        let mut pos = Vec::<f64>::new();
+        let vel = Vec::<f64>::new();
+
+        let pv :Vec<f64> = 
+            pos.iter()
+            .zip(vel)
+            .map(|(p, v)| p + v)
+            .collect();
+
+    }
+
+    fn step_pbest(&mut self) 
+    {
+        if self.pbest_pos > self.pos
+        {
+            self.pbest_pos = self.pos.clone();
+        }
+    }
+}
+
 /// # 群れ
 /// # field
 /// * `particles` - 個体群：
@@ -36,40 +92,40 @@ struct Pso
     particles :Vec<Particle>,
     t :i32,
     gbest_f :f64,
-    gbest_pos :Vec<f64>,
+    gbest_pos :Option<Vec<f64>>,
 }
 impl Pso
 {
-    fn particle_new(n_number:usize) 
-    {
-        let feval = 0.0_f64;
-        let mut vel = Vec::<f64>::with_capacity(n_number);
-        let mut pos = Vec::<f64>::with_capacity(n_number);
-        let mut init_vel = Vec::<f64>::with_capacity(n_number);
-        let mut init_pos = Vec::<f64>::with_capacity(n_number);
-        let mut pbest_f = 0.0_f64;
-        let mut pbest_pos = Vec::<f64>::with_capacity(n_number);
 
-        for _ in 0..n_number  
-        {
-            let mut rng = rand::thread_rng(); 
-            let rand: f64 = rng.gen();
-            vel.push(0.0);
-            pos.push(0.0);
-            init_vel.push(vec![rand ; n_number]);
-            init_pos.push(vec![rand ; n_number]);
-            pbest_pos.push(0.0);
-        }
-    }
-    
-    fn step_opt(n_number:usize)
+    fn step_opt(&mut self, param :&PsoParam)
     {
-        let mut rng = rand::thread_rng(); 
-        let rand: i32 = rng.gen();
+        let mut rng = rand::thread_rng();
+        let w = &param.w;
+        let c1 = &param.c1;
+        let c2 = &param.c2;
+        let gbest = std::mem::replace(&mut self.gbest_pos, None).unwrap();
 
+        self.particles.iter_mut().for_each(
+            |p|
+            {
+                let (current_x, v, pbest_x) = p.ref_mut();
+
+                current_x.iter()
+                    .zip(v.iter_mut())
+                    .zip(pbest_x.iter())
+                    .zip(gbest.iter())
+                    .for_each(| ( ( (x, v), px), gx) |
+                    	*v = w**v +
+                        	rng.gen::<f64>()*c1*(*px - *x) +
+                            rng.gen::<f64>()*c2*(*gx - *x) )
+            }
+        );
+        let _ = std::mem::replace(&mut self.gbest_pos, Some(gbest));
+
+        /*
         for i in 0..n_number
         {
-            vel[i + 1] = interia * vel[i] + acc1_coef * rand * (pbest_pos[i] - pos[i]) + acc2_coef * rand *(gbest_pos[i] - pos[i]);
+            vel[i + 1] = interia * vel[i] + acc1_coef * rng.gen::<f64>() * (pbest_pos[i] - pos[i]) + acc2_coef * rand *(gbest_pos[i] - pos[i]);
             pos[i + 1] = pos[i] + vel[i + 1];
 
         }
@@ -95,11 +151,9 @@ impl Pso
                     gbest_f[n] = pos[i + i * n];                    
                 }    
             }
-            
-        }
-
+        }*/
     }
-    
+
     fn opt()
     {//全体の更新
     
@@ -108,21 +162,127 @@ impl Pso
 }
 /// # パラメータ
 /// # field
-/// * `t_max` - 最大更新回数
-/// * `n_number` - 個体数
-/// * `inertia` -慣性重み 
-/// * `acc_coef` - 加速係数
-/// * `N` - 次元
+/// * `m_iter` - 最大更新回数
+/// * `n_particle` - 個体数
+/// * `w` -慣性重み
+/// * `c1` - 加速係数
+/// * `c2` - 加速係数
+/// * `dim` - 次元
 struct PsoParam
 {
-    t_max :i32,
-    n_number :usize,
-    inertia :f64,
-    acc1_coef :f64,
-    acc2_coef :f64,
-    N : i32,
+    m_iter :u32,
+    n_particle :usize,
+    w :f64,
+    c1 :f64,
+    c2 :f64,
+    dim :u32,
 }
 
 fn main() {
     println!("Hello, world!");
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rand() {
+        let mut rng = rand::thread_rng();
+        let mut count = 0_usize;
+        let rand :i32 = rng.gen();
+
+        println!("({ })bind value = { }", count, rand);
+        count += 1;
+        println!("({ })bind value = { }", count, rand);
+        count += 1;
+        println!("({ })bind value = { }", count, rand);
+        count += 1;
+
+        println!("({ })random value = { }", count, rng.gen::<f64>());
+        count += 1;
+        println!("({ })random value = { }", count, rng.gen::<f64>());
+        count += 1;
+        println!("({ })random value = { }", count, rng.gen::<f64>());
+        count += 1;
+    }
+
+    #[test]
+    fn test_for_each() {
+        let mut vec = vec![1, 2, 3, 4, 5];
+        println!("{:?}", vec);
+
+        vec.iter_mut().for_each(|x| *x *= 2);
+        println!("{:?}", vec);
+
+        vec.iter().for_each(|x| println!("{ }", x));
+    }
+
+    #[test]
+    fn test_iter_zip() {
+        let a1 = [1, 2, 3];
+        let a2 = [4, 5, 6, 7];
+        let a3 = [8, 9];
+
+        let mut iter = a1.iter();
+        for _ in 0..5
+        {
+            println!("{:?}", iter.next());
+        }
+        println!("---------------------------");
+        let mut iter = a1.iter();
+        while let Some(x) = iter.next()
+        {
+            println!("{:?}", x);
+        }
+        println!("---------------------------");
+
+        let mut iter = a1.iter().zip(a2.iter());
+        for _ in 0..5
+        {
+            println!("{:?}", iter.next());
+        }
+        println!("---------------------------");
+        let mut iter = a1.iter().zip(a2.iter());
+        while let Some(x) = iter.next()
+        {
+            println!("{:?}", x);
+        }
+        println!("---------------------------");
+
+        let mut iter = a1.iter().zip(a2.iter()).zip(a3.iter());
+        for _ in 0..5
+        {
+            println!("{:?}", iter.next());
+        }
+        println!("---------------------------");
+        let mut iter = a1.iter().zip(a2.iter()).zip(a3.iter());
+        while let Some(x) = iter.next()
+        {
+            println!("{:?}", x);
+        }
+        println!("---------------------------");
+
+    }
+
+    #[test]
+    fn test_sum_vec() {
+        let mut a1 = vec![1, 2, 3, 4, 5, 7];
+        let a2 = vec![4, 5, 6, 7, 8, 9];
+
+        let sum :Vec<_> = 
+            a1.iter()
+            .zip(a2.iter())
+            .map(|(x, y)| x + y)
+            .collect()
+            ;
+        println!("map:{:?}",sum);
+
+        a1.iter_mut()
+            .zip(a2.iter())
+            .for_each(|(x, y)| *x += *y)
+            ;
+        println!("for each:{:?}",a1);
+    }
 }
